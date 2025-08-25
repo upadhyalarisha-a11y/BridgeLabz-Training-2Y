9459eb39956a4cf4dc5475f2f6c1fec133e1f1e5
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-08-25 10:57:51 +0530

    Adding Smallest Check Question

---
+++ Programming-Elements/Level-1/SmallestCheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SmallestCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input 3 numbers
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int number3 = scanner.nextInt();
+
+        // Check if first is the smallest
+        if (number1 < number2 && number1 < number3) {
+            System.out.println("Is the first number the smallest? Yes");
+        } else {
+            System.out.println("Is the first number the smallest? No");
+        }
+
+        scanner.close();
+    }
+}
+
